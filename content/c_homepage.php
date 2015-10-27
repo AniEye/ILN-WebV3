bbe83870ba897312186559c49560501f52022e04
Andre Laugaland <laugaland_6@hotmail.com>
2015-10-27 18:19:18 +1000

    home fixed

--- content/c_homepage.php
+++ content/c_homepage.php
@@ -1,23 +1,20 @@
-<p>
-<table>
-	<tr>
-		<td rowspan="2">
-			<img height="250px" width="250px" src="img/homepage_pic.jpg"/>
-		</td>
-		<td>
-			<h2>LIS EDUCATION: THROUGH TIME AND SPACE</h2>
-		</td>
-	</tr>
-	<tr>
-		<td class="text">
-			<p>
-				While doing some research for this discussion topic, I came across this very comprehensive 
-				look at the development of LIS education around the world. Presented as a slideshow, this 
-				takes quite some time to read from start to finish, but even a quick flick through it shows 
-				a wide variety of ways to become a qualified librarian. It is worth sharing just to acknowledge 
-				the huge amount of research that must have gone into creating this!
-			</p>
-		</td>
-	</tr>				
-</table>
-</p>
+
+<div class="row">
+	<div class="col-md-12 center-align">
+		<h3>LIS EDUCATION: THROUGH TIME AND SPACE</h3>
+	</div>
+						
+	<div class="col-md-2">
+		<img height="250px" width="250px" src="img/homepage_pic.jpg"/>
+	</div>
+	<div class="col-md-2"></div>
+	<div class="col-md-8">
+		<p>
+			While doing some research for this discussion topic, I came across this very comprehensive 
+			look at the development of LIS education around the world. Presented as a slideshow, this 
+			takes quite some time to read from start to finish, but even a quick flick through it shows 
+			a wide variety of ways to become a qualified librarian. It is worth sharing just to acknowledge 
+			the huge amount of research that must have gone into creating this!
+		</p>
+	</div>
+</div>
