52a374399e80da621b8a7fd4fc69529ee4dbca77
AliShazada <aliahmadshahzada@gmail.com>
2015-10-27 17:59:16 +1000

    ..

--- content/c_meet_our_team.php
+++ content/c_meet_our_team.php
@@ -3,12 +3,12 @@
 	<h3></h3>
 	<img src="img\team.jpg" alt="team" >
 	<p>  </p>
-	<p>The International Librarians Network is run by a dedicated team of volunteers from around the globe. Our volunteers donate not only their time but their valuable expertise to ensure the ILN remains a global leader in international peer-mentoring. Without the work of all of our volunteers, the ILN simply could not happen.
+	<p >The International Librarians Network is run by a dedicated team of volunteers from around the globe. Our volunteers donate not only their time but their valuable expertise to ensure the ILN remains a global leader in international peer-mentoring. Without the work of all of our volunteers, the ILN simply could not happen.
 	</p>
 	<p>Find out more about our volunteers and meet the:
 	</p>
 		<ul>
-		<li><a href="http://interlibnet.org/about/meet-our-team/iln-directors/">ILN Directors</a></li>
+		<li><a href="ILN_directors.php">ILN Directors</a></li>
 		<li><a href="http://interlibnet.org/about/meet-our-team/iln-ambassadors/">ILN Ambassadors</a></li>
 		<li><a href="http://interlibnet.org/about/meet-our-team/iln-coordinators-and-consultants/">ILN Coordinators and Consultants</a></li>
 		<li><a href="http://interlibnet.org/about/meet-our-team/iln-board/">ILN Board</a></li>
