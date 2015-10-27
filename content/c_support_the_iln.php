9a51e3dcdf81b4228f39cec7471c9a138e4ab4cb
AliShazada <aliahmadshahzada@gmail.com>
2015-10-27 14:29:33 +1000

    Support the ILN Page

---
+++ content/c_support_the_iln.php
@@ -0,0 +1,19 @@
+<p>
+	<h1>SUPPORT THE ILN</h1>
+	<p>The International Librarians Network is a non-profit organisation run entirely by volunteers. The ILN is an incorporated association in New South Wales (NSW), Australia and is regulated by NSW Fair Trading under the Associations Incorporation Act 2009.
+	</p>
+	<p>Created and run by librarians for librarians, the ILN could not exist without the generous donations of time, energy and finance made by our volunteers, participants and the ILN community.
+	</p>
+	<p>By donating to the ILN you help us keep the program free for participants all around the world. You are also helping secure the financial and operational sustainability of the program for the future. All donations help, big or small.
+	</p>
+	<p>It’s up to each of the participants involved to make the time to get to know their ILN partner. Distance communication is tricky, especially when you are just getting to know someone, so it is important to communicate clearly as well as to set expectations for the partnership. Tips we can suggest in the early stages of a partnership are:
+	</p>
+	<h3> 
+	</h3>
+	<input type="image" id="button"  src="img\Donate_Button.gif" />	
+	<h3>Sponsorship and partnership opportunities</h3>
+	<p>Organisations and institutions can also support the ILN by taking up one of our sponsorship or partnership packages. For more information on these packages, please contact us at contact@interlibnet.org.
+	</p>
+		
+		
+</p>
