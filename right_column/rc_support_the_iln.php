79f64a9826369d6d7510fc0192ee62ebae90d096
AliShazada <aliahmadshahzada@gmail.com>
2015-10-27 20:09:10 +1000

    right column added in support column

--- right_column/rc_support_the_iln.php
+++ right_column/rc_support_the_iln.php
@@ -1,3 +1,9 @@
 <p>
-	this is the possibility of a right column on the homepage
+	<h3></h3>
+	<h4>WELCOME TO THE INTERNATIONAL LIBRARIANS NETWORK</h4>
+	<p>A meeting place for librarians from around the world.
+	</p>
+	<p><strong>SUPPORT THE ILN </strong></p>
+	<input type="image" id="button"  src="img\Donate_Button.gif" />	
+	<p>By donating to the ILN you help us keep the program free for participants all around the world. You are also helping secure the financial and operational sustainability of the program for the future. All donations help, big or small.</p>
 </p>
