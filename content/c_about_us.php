1dbe2a6982981bd9bdc64780954482580c2bf6dc
AliShazada <aliahmadshahzada@gmail.com>
2015-10-27 15:03:05 +1000

    About us page
    About us page complete m8

--- content/c_about_us.php
+++ content/c_about_us.php
@@ -1,3 +1,11 @@
 <p>
-	content in about us
+	<h1>ABOUT THE ILN</h1>
+	<p>The International Librarians Network (ILN) <a href="http://interlibnet.org/frequently-asked-questions-2/#1">peer mentoring</a> program is a facilitated program aimed at helping librarians develop international networks. We believe that innovation and inspiration can cross borders, and that spreading our networks beyond our home countries can make us better at what we do.
+	</p>
+	<p>Participants in the program are matched with others outside their country, based on the information they provide to the program coordinators. Partnerships are made for a fixed term, and during this period the partnerships are supported by regular contact and discussion points led by the program coordinators. Supported partnerships have an end date, however it is our vision that participants would develop a widening network of ongoing, independent professional relationships.
+	</p>
+	<p>The International Librarians Network is <a href="http://interlibnet.org/about/meet-our-team/">run by volunteers</a> all around the world.
+	</p>
+	<p>While the ILN enjoys the support of many esteemed professional and commercial bodies worldwide, it is run independently.
+	</p>
 </p>
